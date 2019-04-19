07ec5342602320bb57fd018576f0acf2fce29375
Calvin Cheng <linchuan.cheng@gmail.com>
2019-04-19 22:31:31 +0800

    1.0.14

--- src/__tests__/keypair.test.ts
+++ src/__tests__/keypair.test.ts
@@ -5,17 +5,12 @@ const ed25519 = forge.pki.ed25519
 
 test('KeyPair', () => {
     const seed = forge.random.getBytesSync(32)
-        const seedBuffer = Buffer.from(seed, 'binary')
-        const kp = ed25519.generateKeyPair({ seed })
-        const publicKeyBuffer = forge.util.createBuffer(kp.publicKey)
-        const publicKeyHex = forge.util.bytesToHex(publicKeyBuffer.data)
-        const privateKeyBuffer = forge.util.createBuffer(kp.privateKey)
-        const privateKeyHex = forge.util.bytesToHex(privateKeyBuffer.data)
-        const keypair = new KeyPair(
-            publicKeyHex,
-            privateKeyHex,
-            false,
-            seedBuffer
-        )
+    const seedBuffer = Buffer.from(seed, 'binary')
+    const kp = ed25519.generateKeyPair({ seed })
+    const publicKeyBuffer = forge.util.createBuffer(kp.publicKey)
+    const publicKeyHex = forge.util.bytesToHex(publicKeyBuffer.data)
+    const privateKeyBuffer = forge.util.createBuffer(kp.privateKey)
+    const privateKeyHex = forge.util.bytesToHex(privateKeyBuffer.data)
+    const keypair = new KeyPair(publicKeyHex, privateKeyHex, false, seedBuffer)
     expect(keypair.getSeed()!.length).toBe(32)
 })
