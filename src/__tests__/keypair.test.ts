e62f7337c10ecb08b723c904a2c27a8d835ab31f
Calvin Cheng <linchuan.cheng@gmail.com>
2019-04-19 22:31:20 +0800

    Update KeyPair test

--- src/__tests__/keypair.test.ts
+++ src/__tests__/keypair.test.ts
@@ -1,6 +1,21 @@
+import forge from 'node-forge'
 import KeyPair from '../keypair'
 
+const ed25519 = forge.pki.ed25519
+
 test('KeyPair', () => {
-    const keyPair = new KeyPair()
-    expect(keyPair).toBe(keyPair)
+    const seed = forge.random.getBytesSync(32)
+        const seedBuffer = Buffer.from(seed, 'binary')
+        const kp = ed25519.generateKeyPair({ seed })
+        const publicKeyBuffer = forge.util.createBuffer(kp.publicKey)
+        const publicKeyHex = forge.util.bytesToHex(publicKeyBuffer.data)
+        const privateKeyBuffer = forge.util.createBuffer(kp.privateKey)
+        const privateKeyHex = forge.util.bytesToHex(privateKeyBuffer.data)
+        const keypair = new KeyPair(
+            publicKeyHex,
+            privateKeyHex,
+            false,
+            seedBuffer
+        )
+    expect(keypair.getSeed()!.length).toBe(32)
 })
